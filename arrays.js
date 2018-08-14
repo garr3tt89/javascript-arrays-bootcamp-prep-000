23149e49d40b2a87b3927ca3fdd9ae7a04b9f814
garr3tt1989@gmail.com Hausman <garr3tt1989@gmail.com>
2018-08-14 17:19:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,32 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(array, element){
+  var array = "foo"
+  var element = 1
+return [array, element]
+  var new_array = [element, array[0]]
+  return new_array
+}
+function descructivelyAddElementToBeginningOfArray(array, element){
+ return array, element
+function descructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
+   return array[2]
+}
+function removeElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+function removeElementFromEndOfArray(array){
+  array.pop()
+  return array
